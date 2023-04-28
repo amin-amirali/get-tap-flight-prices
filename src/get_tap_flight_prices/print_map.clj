2535ad8ceb6cebb2bd9b4e71cf6142b45ace75e3
Amin Amirali <amin.amirali@telenordigital.com>
2023-04-28 22:55:58 +0200

    Make it easier to use print_map

--- src/get_tap_flight_prices/print_map.clj
+++ src/get_tap_flight_prices/print_map.clj
@@ -1,12 +1,15 @@
 (ns get-tap-flight-prices.print-map)
 
-(defn list-of-maps-as-tsv-with-current-timestamp
+(def all-keys
+  [:departureDate :arrivalDate :outFareFamily :price :from :to])
+
+(defn as-tsv-with-current-timestamp
   "Receives a list of maps, outputs these maps as tsv, following the ordered keys. Current time is also
   included at the end of each of the row."
-  [input-list ordered-keys]
+  [input-list]
   (let [startdttm (.format (java.text.SimpleDateFormat. "yyyy/MM/dd HH:mm") (new java.util.Date))]
     (->> input-list
          (map #(assoc % :extraction_date startdttm))
-         (map #(select-keys % (merge ordered-keys :extraction_date)))
+         (map #(select-keys % (merge all-keys :extraction_date)))
          (map vals)
          (map #(println (apply str (interpose "\t" %)))))))
